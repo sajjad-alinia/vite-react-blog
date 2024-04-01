0ac254786fca2975f7604ce2dee5ef108f6be2ec
sajjad alinia <77253857+sajjad-alinia@users.noreply.github.com>
2024-04-01 16:53:04 +0330

    Add version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return <div className="flex w-full p-5 border-t text-gray-400">this is footer :)</div>;
+  return (
+    <div className="flex-between-center w-full p-5 border-t text-gray-400">
+      <span>this is footer :)</span>
+      <span>version: {import.meta.env.VITE_APP_VERSION}</span>
+    </div>
+  );
 };
 
 export default Footer;
